292eb831eb06785fdd5028021ea1e460c3b98e88
Alexander Simko <alexander@simko.se>
2017-12-13 15:04:47 +0100

    initial commit

---
+++ src/main/Reversi.java
@@ -0,0 +1,12 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Reversi extends PApplet {
+	
+	
+	public static void main(String[] args) {
+		PApplet.main("main.Reversi");
+	}
+
+}
